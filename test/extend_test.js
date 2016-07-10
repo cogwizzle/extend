8166afbf4816d97eff9467fcc83bd91f000f6b56
Joseph Fehrman <joseph.fehrman@gmail.com>
2016-07-10 22:14:06 +0000

    Updated the language of the test file to read more as a requirement.

--- test/extend_test.js
+++ test/extend_test.js
@@ -13,12 +13,12 @@ QUnit.module("Extend", {
  }
 });
 
-QUnit.test("Test that values from objectB is included in objectA after objectA extends objectB", function(assert){
+QUnit.test("Extend shall have the ability to append new properties.", function(assert){
   extend(objectA, objectB);
   assert.deepEqual(objectA, objectB, "Expected that objectA has objectB's values.");
 });
   
-QUnit.test("Test that objectB maintains extra attributes when extended by objectA.", function(assert){
+QUnit.test("Extend shall be able to override previously existing properties.", function(assert){
   var expectedResults = {"x" : "x", "y" : "y"};
   extend(objectB, objectA);
   assert.deepEqual(objectB, expectedResults, "Expected that objectB's x value will be objectA's x value.")
